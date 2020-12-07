033517da19a752b7f0b95f7517a909648e82ca7c
euzivam junior <e.sousasilvajunior@dxc.com>
2020-12-07 14:37:13 -0300

    Versão 2 da minha solução

--- oo/carro_minha_solucao.py
+++ oo/carro_minha_solucao.py
@@ -1,23 +1,102 @@
+"""
+  #Testando motor
+  >>> motor = Motor()
+  >>> motor.velocidade
+  0
+  >>> motor.acelerar()
+  >>> motor.velocidade
+  1
+  >>> motor.acelerar()
+  >>> motor.velocidade
+  2
+
+  >>> motor.acelerar()
+  >>> motor.velocidade
+  3
+  >>> motor.frear()
+  >>> motor.velocidade
+  1
+  >>> motor.frear()
+  >>> motor.velocidade
+  0
+
+  # Testando Direção()
+  >>> direcao = Direcao()
+  >>> direcao.direcao
+  'Norte'
+  >>> direcao.girar_a_direita()
+  >>> direcao.direcao
+  'Leste'
+  >>> direcao.girar_a_direita()
+  >>> direcao.direcao
+  'Sul'
+  >>> direcao.girar_a_direita()
+  >>> direcao.direcao
+  'Oeste'
+  >>> direcao.girar_a_direita()
+  >>> direcao.direcao
+  'Norte'
+  >>> direcao.girar_a_esquerda()
+  >>> direcao.direcao
+  'Oeste'
+  >>> direcao.girar_a_esquerda()
+  >>> direcao.direcao
+  'Sul'
+   >>> direcao.girar_a_esquerda()
+  >>> direcao.direcao
+  'Leste'
+   >>> direcao.girar_a_esquerda()
+  >>> direcao.direcao
+  'Norte'
+
+  # Testando Carro
+  >>> carro = Carro(direcao, motor)
+  >>> carro.calcular_velocidade()
+  0
+  >>> carro.acelerar()
+  >>> carro.calcular_velocidade()
+  1
+  >>> carro.acelerar()
+  >>> carro.calcular_velocidade()
+  2
+  >>> carro.acelerar()
+  >>> carro.calcular_velocidade()
+  3
+  >>> carro.calcular_direcao()
+  'Norte'
+  >>> carro.girar_a_direita()
+  >>> carro.calcular_direcao()
+  'Leste'
+  >>> carro.girar_a_esquerda()
+  >>> carro.calcular_direcao()
+  'Norte'
+  >>> carro.girar_a_esquerda()
+  >>> carro.calcular_direcao()
+  'Oeste'
+"""
+
 class Carro:
     def __init__(self, direcao, motor):
-        self.calcular_velocidade = motor.velocidade
-        self.calcular_direcao = direcao.direcao
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
     def acelerar(self):
-        motor.acelerar()
-        self.calcular_velocidade = motor.velocidade
+        self.motor.acelerar()
 
     def frear(self):
-        motor.frear()
-        self.calcular_velocidade = motor.velocidade
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.direcao
 
     def girar_a_direita(self):
-        direcao.girar_a_direita()
-        self.calcular_direcao = direcao.direcao
+        self.direcao.girar_a_direita()
 
     def girar_a_esquerda(self):
-        direcao.girar_a_esquerda()
-        self.calcular_direcao = direcao.direcao
+        self.direcao.girar_a_esquerda()
 
 
 class Motor:
@@ -90,15 +169,15 @@ if __name__ == '__main__':
 
     print('\nTestando métodos e atributos Carro')
     carro.acelerar()
-    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(1, carro.calcular_velocidade))
+    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(1, carro.calcular_velocidade()))
     carro.acelerar()
-    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(2, carro.calcular_velocidade))
+    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(2, carro.calcular_velocidade()))
     carro.frear()
-    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(0, carro.calcular_velocidade))
-    print('Valor esperado direção: {} | Valor obtido: {}'.format('Norte', carro.calcular_direcao))
+    print('Valor esperado velocidade: {} | Valor obtido: {}'.format(0, carro.calcular_velocidade()))
+    print('Valor esperado direção: {} | Valor obtido: {}'.format('Norte', carro.calcular_direcao()))
     carro.girar_a_direita()
-    print('Valor esperado direção: {} | Valor obtido: {}'.format('Leste', carro.calcular_direcao))
+    print('Valor esperado direção: {} | Valor obtido: {}'.format('Leste', carro.calcular_direcao()))
     carro.girar_a_esquerda()
-    print('Valor esperado direção: {} | Valor obtido: {}'.format('Norte', carro.calcular_direcao))
+    print('Valor esperado direção: {} | Valor obtido: {}'.format('Norte', carro.calcular_direcao()))
     carro.girar_a_esquerda()
-    print('Valor esperado direção: {} | Valor obtido: {}'.format('Oeste', carro.calcular_direcao))
+    print('Valor esperado direção: {} | Valor obtido: {}'.format('Oeste', carro.calcular_direcao()))
